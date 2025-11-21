87140accc26380eac37a70150ebf0f6d43464bfb
Gabriel Volpe <volpegabriel@gmail.com>
2025-11-21 09:42:24 +0100

    delete old site version

--- js/email.js
+++
@@ -1,7 +0,0 @@
-function showContact() {
-  document.querySelector("dialog").showModal();
-  let elem = document.getElementById("dec-contact");
-  let email = atob("aGVsbG9AZ3ZvbHBlLmNvbQo=");
-  elem.text = email;
-  elem.href = "mailto:" + email;
-}

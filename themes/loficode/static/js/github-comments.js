1c3a6d34180bb965fb268c376185049efba660bc
Gabriel Volpe <volpegabriel@gmail.com>
2026-08-04 17:17:19 +0200

    fix github comments style

--- themes/loficode/static/js/github-comments.js
+++ themes/loficode/static/js/github-comments.js
@@ -11,28 +11,41 @@ function ShowComments(repo_name, comment_id, page_id)
         success: function(comments, textStatus, jqXHR) {
 
             if (1 == page_id) {
+                $("#gh-comments-list").empty();
+
                 // post button
                 var url = "https://github.com/" + repo_name + "/issues/" + comment_id + "#new_comment_field";
-              $("#gh-comments-list").append("<form action='" + url + "' rel='nofollow' target='_blank'> <div class='load-more-section'> <input type='submit' class='gh-post-btn' value='Post a comment on Github' /> </div> </form>");
+                $("#gh-comments-list").append("<div class='gh-comments-actions'><a class='gh-post-btn' href='" + escapeAttr(url) + "' rel='nofollow noopener noreferrer' target='_blank'>Post a comment on GitHub</a></div>");
+
+                if (0 == comments.length) {
+                    $("#gh-comments-list").append("<div class='gh-comments-empty'>No comments yet.</div>");
+                }
             }
 
             // Individual comments
             $.each(comments, function(i, comment) {
 
                 var date = new Date(comment.created_at);
+                var dateText = date.toLocaleString(undefined, {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit"
+                });
 
-                var t = "<div id='gh-comment'>";
+                var t = "<article class='gh-comment'>";
                 t += "<div class='gh-comment-author'>";
-                t += "<img src='" + comment.user.avatar_url + "' width='24px'>";
-                t += "<b><a href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>";
-                t += " posted at ";
-                t += "<em>" + date.toUTCString() + "</em>";
+                t += "<img src='" + escapeAttr(comment.user.avatar_url) + "' alt='' loading='lazy'>";
+                t += "<div class='gh-comment-meta'>";
+                t += "<b><a href='" + escapeAttr(comment.user.html_url) + "' rel='noopener noreferrer' target='_blank'>" + escapeHtml(comment.user.login) + "</a></b>";
+                t += "<time datetime='" + escapeAttr(comment.created_at) + "' title='" + escapeAttr(date.toUTCString()) + "'>" + escapeHtml(dateText) + "</time>";
                 t += "</div>";
-                t += "<div id='gh-comment-hr'></div>";
+                t += "</div>";
                 t += "<div class='gh-comment-body'>";
-                t += comment.body_html;
+                t += cleanGithubCommentHtml(comment.body_html || escapeHtml(comment.body || ""));
                 t += "</div>";
-                t += "</div>";
+                t += "</article>";
                 $("#gh-comments-list").append(t);
             });
 
@@ -52,9 +65,51 @@ function ShowComments(repo_name, comment_id, page_id)
             }
         },
         error: function() {
-            $("#gh-comments-list").append("Comments are not open for this post yet.");
+            $("#gh-comments-list").html("<div class='gh-comments-error'>Comments are not available for this post yet.</div>");
         }
     });
+}
+
+function escapeHtml(value)
+{
+    return String(value || "").replace(/[&<>"']/g, function(char) {
+        return {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': "&quot;",
+            "'": "&#39;"
+        }[char];
+    });
+}
+
+function escapeAttr(value)
+{
+    return escapeHtml(value);
+}
+
+function cleanGithubCommentHtml(value)
+{
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = value || "";
+
+    [
+        ".email-hidden-toggle",
+        ".email-hidden-reply",
+        ".email-quoted-reply"
+    ].forEach(function(selector) {
+        Array.prototype.forEach.call(wrapper.querySelectorAll(selector), function(element) {
+            element.remove();
+        });
+    });
+
+    Array.prototype.forEach.call(wrapper.querySelectorAll(".email-fragment"), function(element) {
+        if (!element.textContent.trim() && 0 == element.children.length) {
+            element.remove();
+        }
+    });
+
+    return wrapper.innerHTML;
 }
 
 function DoGithubComments(repo_name, comment_id)
